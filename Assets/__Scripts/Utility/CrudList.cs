bce587eb20065336e12469ed78802bf886cf97e2
sztynksior <szszedel@gmail.com>
2023-11-03 20:28:49 +0100

    Doxygen comment utility logic scripts

--- Assets/__Scripts/Utility/CrudList.cs
+++ Assets/__Scripts/Utility/CrudList.cs
@@ -5,27 +5,51 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
+/// <summary>
+/// Represents a generic list with basic CRUD operations.
+/// </summary>
 public class CrudList<T> : IEnumerable<T>
 {
     private List<T> items;
+
+    /// <summary>
+    /// Gets the count of items in the list.
+    /// </summary>
     public int Count => items.Count;
 
     DefaultLoader<T> loader = new DefaultLoader<T>();
+
+    /// <summary>
+    /// Initializes a new instance of the CrudList.
+    /// </summary>
     public CrudList()
     {
         items = new List<T>();
     }
 
+    /// <summary>
+    /// Loads a list from a given path.
+    /// </summary>
+    /// <param name="path">The path to load the list from.</param>
     public void LoadList(string path)
     {
         items = loader.Load(path);
     }
 
+    /// <summary>
+    /// Loads a list from a given label reference.
+    /// </summary>
+    /// <param name="label">The label reference to load the list from.</param>
     public void LoadList(AssetLabelReference label)
     {
         items = loader.Load(label);
     }
 
+    /// <summary>
+    /// Gets an item from the list by its index.
+    /// </summary>
+    /// <param name="itemID">The index of the item to retrieve.</param>
+    /// <returns>The item at the specified index or a default value if not found.</returns>
     public T GetItem(int itemID)
     {
         if (IsInListRange(itemID))
@@ -36,28 +60,45 @@ public class CrudList<T> : IEnumerable<T>
         return items.ElementAt(itemID);
     }
 
+    /// <summary>
+    /// Gets a random item from the list.
+    /// </summary>
+    /// <returns>A randomly selected item from the list.</returns>
     public T GetRandomItem()
     {
         int index = UnityEngine.Random.Range(0, items.Count);
         return items[index];
     }
 
+    /// <summary>
+    /// Adds an item to the list.
+    /// </summary>
+    /// <param name="item">The item to add to the list.</param>
     public void AddItem(T item)
     {
         items.Add(item);
     }
 
-    public void UpdateItem(T newItem, int cardId)
+    /// <summary>
+    /// Updates an item in the list at the specified index.
+    /// </summary>
+    /// <param name="newItem">The new item to replace the existing one.</param>
+    /// <param name="itemID">The index of the item to update.</param>
+    public void UpdateItem(T newItem, int itemID)
     {
-        if (IsInListRange(cardId))
+        if (IsInListRange(itemID))
         {
-            Debug.LogError($"Couldn't update item with id - {cardId}");
+            Debug.LogError($"Couldn't update item with id - {itemID}");
             return;
         }
 
-        items[cardId] = newItem;
+        items[itemID] = newItem;
     }
 
+    /// <summary>
+    /// Removes an item from the list by its index.
+    /// </summary>
+    /// <param name="itemId">The index of the item to remove.</param>
     public void RemoveItem(int itemId)
     {
         if (IsInListRange(itemId))
@@ -69,6 +110,10 @@ public class CrudList<T> : IEnumerable<T>
         items.RemoveAt(itemId);
     }
 
+    /// <summary>
+    /// Removes an item from the list.
+    /// </summary>
+    /// <param name="item">The item to remove from the list.</param>
     public void Remove(T item)
     {
         int index = items.IndexOf(item);
@@ -80,7 +125,6 @@ public class CrudList<T> : IEnumerable<T>
         {
             Debug.LogError("Item not found in the list.");
         }
-
     }
 
     private bool IsInListRange(int itemId)
--- Assets/__Scripts/Utility/SceneLoader.cs
+++ Assets/__Scripts/Utility/SceneLoader.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections;
 using UnityEngine;
 
+/// <summary>
+/// Enum representing different scenes in the game.
+/// </summary>
 public enum SceneEnum
 {
     MainMenu,
@@ -9,17 +12,39 @@ public enum SceneEnum
     EndMenu
 }
 
-
+/// <summary>
+/// Handles scene loading and transitions with fade effects.
+/// </summary>
 public class SceneLoader : Singleton<SceneLoader>
 {
+    /// <summary>
+    /// Action triggered when the scene changes.
+    /// </summary>
     public Action OnSceneChanged;
+
+    /// <summary>
+    /// Action triggered when the new scene is fully loaded.
+    /// </summary>
     public Action OnSceneFullyLoaded;
+
+    /// <summary>
+    /// The fade screen used for transitions.
+    /// </summary>
     [SerializeField] FadeScreen fadeScreen;
+
+    /// <summary>
+    /// Loads a scene by its index with fade effects.
+    /// </summary>
+    /// <param name="sceneIndex">The index of the scene to load.</param>
     public void LoadScene(int sceneIndex)
     {
         StartCoroutine(LoadSceneRoutine(sceneIndex));
     }
 
+    /// <summary>
+    /// Loads a scene by its enum value with fade effects.
+    /// </summary>
+    /// <param name="sceneEnum">The enum value representing the scene to load.</param>
     public void LoadScene(SceneEnum sceneEnum)
     {
         int sceneIndex = (int)sceneEnum;
@@ -45,5 +70,6 @@ public class SceneLoader : Singleton<SceneLoader>
         operation.allowSceneActivation = true;
         fadeScreen.FadeIn();
 
+        OnSceneFullyLoaded?.Invoke();
     }
 }
