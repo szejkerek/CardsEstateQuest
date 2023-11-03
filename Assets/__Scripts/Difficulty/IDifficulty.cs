0894eef8c82821cb4dc56b3605f53c72b4f4990d
sztynksior <szszedel@gmail.com>
2023-11-03 19:45:38 +0100

    Doxygen comment Difficulty logic scripts

--- Assets/__Scripts/Difficulty/IDifficulty.cs
+++ Assets/__Scripts/Difficulty/IDifficulty.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// An interface representing a difficulty level.
+/// </summary>
 public interface IDifficulty
 {
+    /// <summary>
+    /// Gets the name of the difficulty level.
+    /// </summary>
     string Name { get; }
+
+    /// <summary>
+    /// Gets the icon associated with the difficulty level.
+    /// </summary>
     Sprite Icon { get; }
+
+    /// <summary>
+    /// Gets the list of parameter rules for the difficulty level.
+    /// </summary>
     List<ParameterRule> CardParameters { get; }
+
+    /// <summary>
+    /// Gets the number of bombs for the difficulty level.
+    /// </summary>
     int NumberOfBombs { get; }
 }
