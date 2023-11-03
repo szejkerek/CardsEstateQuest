287e2af2ad007fb54da698b4457b7d82893b0634
sztynksior <szszedel@gmail.com>
2023-11-03 19:49:56 +0100

    Doxygen comment Grid logic sripts

--- Assets/__Scripts/Grid/GridItem.cs
+++ Assets/__Scripts/Grid/GridItem.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Manages interactions and placement of cards on a grid item.
+/// </summary>
 public class GridItem : MonoBehaviour
 {
     [SerializeField] bool isTaken = false;
     [SerializeField] Material _hoverMaterial;
-    Material _initalMateral;
+    Material _initialMaterial;
     Renderer _renderer;
     bool hoversOver = false;
     GameplayManager manager;
@@ -14,7 +17,7 @@ public class GridItem : MonoBehaviour
     {
         _renderer = GetComponent<Renderer>();
         manager = GameplayManager.Instance;
-        _initalMateral = _renderer.material;
+        _initialMaterial = _renderer.material;
     }
 
     private void OnMouseDown()
@@ -26,23 +29,32 @@ public class GridItem : MonoBehaviour
         isTaken = true;
     }
 
+    /// <summary>
+    /// Handles the mouse hover behavior over the grid item.
+    /// </summary>
     public void OnMouseOver()
     {
         hoversOver = true;
         _renderer.material = _hoverMaterial;
     }
 
+    /// <summary>
+    /// Handles the mouse exit behavior from the grid item.
+    /// </summary>
     public void OnMouseExit()
     {
         hoversOver = false;
-        _renderer.material = _initalMateral;
+        _renderer.material = _initialMaterial;
     }
 
+    /// <summary>
+    /// Places a card on the grid item and sets up necessary components and events.
+    /// </summary>
     public void PlaceCard()
     {
         CardObject card = manager.SelectedCard;
         currentBuilding = Instantiate(card.Model, transform.position, transform.rotation);
-        Transform meshChild = currentBuilding.transform.Find("Mesh"); //String yikes TODO: Zmienić na referencje na postawie modelu
+        Transform meshChild = currentBuilding.transform.Find("Mesh"); // TODO: Replace with a reference based on the model
 
         DestroyWithBomb bomb = meshChild.gameObject.AddComponent<DestroyWithBomb>();
         bomb.BombUsed += OnBombUsed;
@@ -54,6 +66,10 @@ public class GridItem : MonoBehaviour
         GameplayManager.Instance.DeselectCard();
         Debug.Log("Placed");
     }
+
+    /// <summary>
+    /// Handles the event when a bomb is used, destroying the current building.
+    /// </summary>
     public void OnBombUsed()
     {
         if (!GameplayManager.Instance.UseBomb())
--- Assets/__Scripts/Grid/GridManager.cs
+++ Assets/__Scripts/Grid/GridManager.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
+/// <summary>
+/// Manages the creation of a grid of GameObjects within a container.
+/// </summary>
 public class GridManager : MonoBehaviour
 {
-    [Header("Object Proporties")]
+    [Header("Object Properties")]
     [SerializeField] private GameObject prefab;
 
-    [Header("Grid proporties")]
+    [Header("Grid Properties")]
     [SerializeField] private Transform gridContainer;
     [SerializeField] private Vector2Int gridSize;
     [SerializeField] private Vector2 gridSpacing;
@@ -15,7 +18,10 @@ public class GridManager : MonoBehaviour
         GenerateGrid();
     }
 
-    void GenerateGrid()
+    /// <summary>
+    /// Generates a grid of GameObjects based on the specified grid properties.
+    /// </summary>
+    private void GenerateGrid()
     {
         for (int x = 0; x < gridSize.x; x++)
         {
@@ -27,14 +33,23 @@ public class GridManager : MonoBehaviour
             }
         }
     }
+
+    /// <summary>
+    /// Returns the minimum boundary of the grid.
+    /// </summary>
+    /// <returns>The minimum boundary as a Vector3.</returns>
     public Vector3 GetMinBoundary()
     {
-        return new Vector3(0, 0, 0); 
+        return new Vector3(0, 0, 0);
     }
 
+    /// <summary>
+    /// Returns the maximum boundary of the grid.
+    /// </summary>
+    /// <returns>The maximum boundary as a Vector3.</returns>
     public Vector3 GetMaxBoundary()
     {
-        return new Vector3((gridSize.x-1) * gridSpacing.x , 0, (gridSize.y - 1) * gridSpacing.y); 
+        return new Vector3((gridSize.x - 1) * gridSpacing.x, 0, (gridSize.y - 1) * gridSpacing.y);
     }
 
     // Draw Gizmos for the min and max boundaries
