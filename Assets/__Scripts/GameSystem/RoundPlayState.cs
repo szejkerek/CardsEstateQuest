c835713a65498670a4c7c1f39ae7f832460296b0
sztynksior <szszedel@gmail.com>
2023-12-01 23:46:06 +0100

    create IRoundState interface and classes that implement it

---
+++ Assets/__Scripts/GameSystem/RoundPlayState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundPlayState : IRoundState
+{
+    public void Update(RoundManager roundManager)
+    {
+        
+    }
+}
